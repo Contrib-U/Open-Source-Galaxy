ce384fb82e4bc19eafacb15a1bd799956f95b810
kom-senapati <komnoob123@gmail.com>
2024-06-17 10:24:48 +0530

    style: changed font and colors

--- open-source-galaxy/src/components/SearchBar.js
+++ open-source-galaxy/src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 import searchIcon from '../assets/search-icon.png'
+import searchIcon2 from '../assets/icons8-search-50.png'
 
 function SearchBar() {
   return (
@@ -8,7 +9,7 @@ function SearchBar() {
       
         <input type='text' placeholder='Search..'/>
         <button className='search-icon'>
-            <img src={searchIcon}/>
+            <img src={searchIcon2}/>
         </button>
       
     </div>
