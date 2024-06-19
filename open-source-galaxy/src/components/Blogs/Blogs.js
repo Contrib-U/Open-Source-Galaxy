b7a52fdc9f064a6b64e78ec2e39f8290044a8c54
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-06-20 04:56:46 +0530

    Revert "Open/expired links in navbar"

--- open-source-galaxy/src/components/Blogs/Blogs.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Blogs = () => {
-  return <div>Blogs</div>;
-};
-
-export default Blogs;
--- open-source-galaxy/src/components/SearchBar.js
+++ open-source-galaxy/src/components/SearchBar.js
@@ -5,10 +5,10 @@ import searchIcon2 from '../assets/icons8-search-50.png'
 
 function SearchBar() {
   return (
-    <div className='search-bar'>
+    <div className='search-bar' title="search here">
       
-        <input type='text' placeholder='Search..'/>
-        <button className='search-icon'>
+        <input type='text' placeholder='Search...'/>
+        <button className='search-icon' title="click to search">
             <img src={searchIcon2}/>
         </button>
       
