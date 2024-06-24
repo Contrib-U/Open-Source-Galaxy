20aebb77221f87102de04e79d9137c28b44d4041
Deepesh Patil <deepeshvcd6273@gmail.com>
2024-06-24 20:02:54 +0400

    Updated UI
    1. removed the empty `README.md` in `open-source-galaxy`
    2. Updated `App.css`

--- open-source-galaxy/src/components/ElementEven.js
+++ open-source-galaxy/src/components/ElementEven.js
@@ -1,24 +1,20 @@
-import React  from 'react'
-import './ElementEven.css'
+import React from 'react';
+import './ElementEven.css';
 import Email from './Email';
 
 function ElementEven(props) {
   return (
-    <div className='card'>
-        <img src={props.src} alt='MLH' />
-        {/* <div className='row-even'>
-            <p>{props.name} </p>
-            <a href={props.link}>Link</a>
-        </div>
-
-        <Email name = {props.name}/> */}
+    <div class='card-even'>
+      <img src={props.src} alt={props.name} />
+      <div class='card-content'>
         <h3>{props.name}</h3>
-        <div class="buttons">
-            <a href={props.link} class="btn">Link</a>
-            <Email name={props.name} class="btn" />
-        </div>  
+        <div class="buttons-even">
+          <a href={props.link} class="btn-even">Link</a>
+          <Email name={props.name} class="btn-odd" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ElementEven
+export default ElementEven;
